7b31309797ca422b80e568b36a43ea7e91e2462d
qurtex <54555148+qurtex@users.noreply.github.com>
2021-05-27 15:25:03 +0300

    invest added

---
+++ investitions-proj/assets/js/mobile-menu.js
@@ -0,0 +1,11 @@
+function showMMenu() {
+	var mobileContainer = document.getElementById('mobile-menu-container');
+
+	mobileContainer.style.top = '0';
+}
+
+function hideMMenu() {
+	var mobileContainer = document.getElementById('mobile-menu-container');
+
+	mobileContainer.style.top = '-150%';
+}
