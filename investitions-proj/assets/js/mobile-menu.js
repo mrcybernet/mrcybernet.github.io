466a257d02734dae5aae18323ade5454167f173c
qurtex <54555148+qurtex@users.noreply.github.com>
2021-05-28 15:39:14 +0300

    цвф

--- investitions-proj/assets/js/mobile-menu.js
+++ investitions-proj/assets/js/mobile-menu.js
@@ -1,11 +1,9 @@
 function showMMenu() {
 	var mobileContainer = document.getElementById('mobile-menu-container');
-
 	mobileContainer.style.top = '0';
 }
 
 function hideMMenu() {
 	var mobileContainer = document.getElementById('mobile-menu-container');
-
 	mobileContainer.style.top = '-150%';
 }
