562ce98296b66b5cb784de07abc35d861f607188
qurtex <54555148+qurtex@users.noreply.github.com>
2021-05-30 22:03:32 +0300

    added secret preview

---
+++ qmeet-preview/assets/js/mobile-menu.js
@@ -0,0 +1,11 @@
+function hideMobileMenu() {
+	sel = document.getElementById('mobCont');
+
+	sel.style.top = "-150%";
+}
+
+function showMobileMenu() {
+	sel = document.getElementById('mobCont');
+
+	sel.style.top = "0";
+}
