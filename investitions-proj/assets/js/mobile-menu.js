9f49a798ce2a1bde4ccaf380e1d32b0cf0ec5772
qurtex <54555148+qurtex@users.noreply.github.com>
2021-05-31 20:10:19 +0300

    some removes

--- investitions-proj/assets/js/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-function showMMenu() {
-	var mobileContainer = document.getElementById('mobile-menu-container');
-	mobileContainer.style.top = '0';
-}
-
-function hideMMenu() {
-	var mobileContainer = document.getElementById('mobile-menu-container');
-	mobileContainer.style.top = '-150%';
-}
