9cc48329acabdf78b18e5f8f97b0dd062a7ce40b
qurtex <54555148+qurtex@users.noreply.github.com>
2021-06-13 21:29:49 +0300

    TOTAL REDESIGN

--- dolma.makeit/assets/scripts.js
+++
@@ -1,9 +0,0 @@
-function mobShow() {
-	$('.mobile-menu-container').css('display', 'block');
-	$('#desktop-nav').css('display', 'none');
-}
-
-function mobHide() {
-	$('.mobile-menu-container').css('display', 'none');
-	$('#desktop-nav').css('display', 'block');
-}
